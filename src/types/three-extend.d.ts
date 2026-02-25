63af01a49b6038ab4d21b31dcba047c9a1af19db
SUJITH CHALLAe <sujithchalla41@gmail.comm>
2026-02-25 19:13:45 +0530

    Fix TypeScript build errors - add meshline type declarations for Vercel deployment

---
+++ src/types/three-extend.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+import type { Object3DNode, MaterialNode } from '@react-three/fiber';
+import type { MeshLineGeometry, MeshLineMaterial } from 'meshline';
+
+declare module '@react-three/fiber' {
+    interface ThreeElements {
+        meshLineGeometry: Object3DNode<MeshLineGeometry, typeof MeshLineGeometry>;
+        meshLineMaterial: MaterialNode<MeshLineMaterial, typeof MeshLineMaterial>;
+    }
+}
